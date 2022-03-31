a42f4946748faebd111065d6383b85c15f557e61
Cesar Lai <git@cesarlai.com>
2022-03-31 08:57:02 +0800

    initial project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  env: {
+    LOCALE: "zh-CN",
+  },
+};
+
+module.exports = nextConfig;
